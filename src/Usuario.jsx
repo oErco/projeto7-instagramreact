6fb63bf6b5c415ae410e6ac68472a1e4770a9690
guilhermcos <guigc0408@gmail.com>
2023-02-13 12:32:50 -0300

    versão com data-tests mudança de nome e imagem

--- src/Usuario.jsx
+++ src/Usuario.jsx
@@ -7,12 +7,28 @@ export default function Usuario() {
 
     return (
         <div className="user-side-bar">
-                <img onClick={() => setImgPerfil(prompt("digite a url da nova imagem de perfil:"))} src={imgPerfil} alt="" />
+                <img data-test="profile-image" onClick={() => changeImg(setImgPerfil)} src={imgPerfil} alt="" />
                 <div class="user-text">
-                    <p>{username}</p>
-                    <ion-icon onClick={() => setUsername(prompt("Qual seu novo nome de usuário?"))} name="pencil-sharp"></ion-icon>
+                    <p data-test="name">{username}</p>
+                    <ion-icon data-test="edit-name" onClick={() => changeName(setUsername)} name="pencil-sharp"></ion-icon>
                 </div>
             </div>
     )
+}
+
+function changeName(setUsername){
+    const name = prompt("digite a url da nova imagem de perfil:");
+
+    if (name){
+        setUsername(name);
+    }
+}
+
+function changeImg(setImgPerfil){
+    const image = prompt("digite a url da nova imagem de perfil:");
+
+    if (image){
+        setImgPerfil(image);
+    }
 }
 
