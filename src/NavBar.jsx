bcf497209b33cd240cb90ba6e9422a444658429d
guilhermcos <guigc0408@gmail.com>
2023-02-13 15:45:42 -0300

    versão com responsividade renderização navbar pronta

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,29 +1,33 @@
+import { useState } from "react";
+
 export default function Topo() {
-    let mobile = false;
-    if (mobile) {
-        return (
-            <div class="top-bar-mobile">
-                <ion-icon class="icon-instagram" name="logo-instagram"></ion-icon>
-                <img class="logo-instagram" src="assets/logo.png" alt="logo Instagram" />
-                <ion-icon class="direct-icon" name="paper-plane-outline"></ion-icon>
-            </div>
-        )
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    function resizeFunction() {
+        setWidth(window.innerWidth)
     }
-    else {
+
+
+    window.addEventListener("resize", resizeFunction);
+
+
+    let topo;
+    if (width >= 615) {
         return (
-            <div class="externo-top-bar-desktop">
-                <div class="top-bar-desktop">
-                    <div class="top-bar-left-desktop">
-                        <ion-icon class="icon-instagram" name="logo-instagram"></ion-icon>
+            <div className="externo-top-bar-desktop">
+                <div className="top-bar-desktop">
+                    <div className="top-bar-left-desktop">
+                        <ion-icon className="icon-instagram" name="logo-instagram"></ion-icon>
                         <div class="top-left-line"></div>
                         <img class="logo-instagram" src="assets/logo.png" alt="logo Instagram" />
                     </div>
 
-                    <div class="caixa-pesquisa">
+                    <div className="caixa-pesquisa">
                         <input class="pesquisar" value="Pesquisar" type="text" />
                     </div>
 
-                    <div class="top-bar-right-desktop">
+                    <div className="top-bar-right-desktop">
                         <ion-icon class="direct-icon" name="paper-plane-outline"></ion-icon>
                         <ion-icon class="ion-bot" name="compass-outline"></ion-icon>
                         <ion-icon class="ion-bot" name="heart-outline"></ion-icon>
@@ -32,5 +36,52 @@ export default function Topo() {
                 </div>
             </div>
         )
+    } else {
+        return (
+            <div class="top-bar-mobile">
+                <ion-icon class="icon-instagram" name="logo-instagram"></ion-icon>
+                <img class="logo-instagram" src="assets/logo.png" alt="logo Instagram" />
+                <ion-icon class="direct-icon" name="paper-plane-outline"></ion-icon>
+            </div>
+        )
     }
+
+
 }
+
+/*
+if (mobile) {
+    return (
+        <div class="top-bar-mobile">
+            <ion-icon class="icon-instagram" name="logo-instagram"></ion-icon>
+            <img class="logo-instagram" src="assets/logo.png" alt="logo Instagram" />
+            <ion-icon class="direct-icon" name="paper-plane-outline"></ion-icon>
+        </div>
+    )
+}
+else {
+    return (
+        <div class="externo-top-bar-desktop">
+            <div class="top-bar-desktop">
+                <div class="top-bar-left-desktop">
+                    <ion-icon class="icon-instagram" name="logo-instagram"></ion-icon>
+                    <div class="top-left-line"></div>
+                    <img class="logo-instagram" src="assets/logo.png" alt="logo Instagram" />
+                </div>
+
+                <div class="caixa-pesquisa">
+                    <input class="pesquisar" value="Pesquisar" type="text" />
+                </div>
+
+                <div class="top-bar-right-desktop">
+                    <ion-icon class="direct-icon" name="paper-plane-outline"></ion-icon>
+                    <ion-icon class="ion-bot" name="compass-outline"></ion-icon>
+                    <ion-icon class="ion-bot" name="heart-outline"></ion-icon>
+                    <ion-icon class="ion-bot" name="person-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+*/
